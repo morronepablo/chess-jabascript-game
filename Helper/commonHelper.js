2317dbf7030f7cc00d7d20c54f765ec062a64f51
morronepablo <morronepablo@gmail.com>
2023-12-28 01:15:30 -0300

    Parte 30

--- Helper/commonHelper.js
+++ Helper/commonHelper.js
@@ -19,6 +19,21 @@ function checkPieceOfOpponentOnElement(id, color) {
   return false;
 }
 
+// function to check if piece exists of opponent
+function checkPieceOfOpponentOnElementNoDom(id, color) {
+  const opponentColor = color === "white" ? "BLACK" : "WHITE";
+
+  const element = keySquareMapper[id];
+
+  if (!element) return false;
+
+  if (element.piece && element.piece.piece_name.includes(opponentColor)) {
+    return true;
+  }
+
+  return false;
+}
+
 // function to check weather piece exists or not by square-id
 function checkWeatherPieceExistsOrNot(squareId) {
   const square = keySquareMapper[squareId];
@@ -119,6 +134,106 @@ function giveBishopHighlightIds(id) {
   };
 }
 
+function giveBishopCaptureIds(id, color){
+
+  if(!id){
+    return [];
+  }
+
+  let hightlightSquareIds = giveBishopHighlightIds(id);
+
+  let temp = [];
+  const { bottomLeft, topLeft, bottomRight, topRight } = hightlightSquareIds;
+  let returnArr = [];
+
+  // insert into temp
+  temp.push(bottomLeft);
+  temp.push(topLeft);
+  temp.push(bottomRight);
+  temp.push(topRight);
+  
+  for (let index = 0; index < temp.length; index++) {
+    const arr = temp[index];
+
+    for (let j = 0; j < arr.length; j++) {
+      const element = arr[j];
+
+      let checkPieceResult = checkWeatherPieceExistsOrNot(element);
+      if (
+        checkPieceResult &&
+        checkPieceResult.piece &&
+        checkPieceResult.piece.piece_name.toLowerCase().includes(color)
+      ) {
+        break;
+      }
+
+      if (checkPieceOfOpponentOnElementNoDom(element, color)) {
+        returnArr.push(element)
+        break;
+      }
+    }
+  }
+
+ 
+  return returnArr;
+
+}
+
+function giveRookCapturesIds(id,color){
+
+  if(!id)
+  {
+    return [];
+  }
+
+  let hightlightSquareIds = giveRookHighlightIds(id);
+
+  let temp = [];
+  const { bottom, top, right, left } = hightlightSquareIds;
+  let returnArr = [];
+
+  // insert into temp
+  temp.push(bottom);
+  temp.push(top);
+  temp.push(right);
+  temp.push(left);
+  
+  for (let index = 0; index < temp.length; index++) {
+    const arr = temp[index];
+
+    for (let j = 0; j < arr.length; j++) {
+      const element = arr[j];
+
+      let checkPieceResult = checkWeatherPieceExistsOrNot(element);
+      if (
+        checkPieceResult &&
+        checkPieceResult.piece &&
+        checkPieceResult.piece.piece_name.toLowerCase().includes(color)
+      ) {
+        break;
+      }
+
+      if (checkPieceOfOpponentOnElementNoDom(element, color)) {
+        returnArr.push(element)
+        break;
+      }
+    }
+  }
+
+  return returnArr;
+
+}
+
+function giveQueenCapturesIds(id,color){
+
+  if(!id) return [];
+
+  let returnArr = [];
+  returnArr.push(giveBishopCaptureIds(id, color))
+  returnArr.push(giveRookCapturesIds(id, color))
+  return returnArr.flat();
+}
+
 // function to give highlight ids for rook
 function giveRookHighlightIds(id) {
   let finalReturnArray = [];
@@ -193,6 +308,7 @@ function giveRookHighlightIds(id) {
 
 // function to give highlight ids for knight
 function giveKnightHighlightIds(id) {
+
   if (!id) {
     return;
   }
@@ -226,9 +342,7 @@ function giveKnightHighlightIds(id) {
       if (number > 1) {
         finalReturnArray.push(`${alpha}${number - 1}`);
       }
-      // resultArray.push(`${Number(lastElement[1])}`);
-
-      return finalReturnArray;
+      return finalReturnArray
     } else {
       return [];
     }
@@ -351,31 +465,99 @@ function giveKnightHighlightIds(id) {
     }
   }
 
-  return [...top(), ...right(), ...bottom(), ...left()];
+  return [...top(), ...bottom(), ...left(), ...right()];
+}
+
+
+function giveKnightCaptureIds(id, color) {
+
+  if (!id) {
+    return [];
+  }
+
+  let returnArr  = giveKnightHighlightIds(id);
+
+  returnArr = returnArr.filter(element => {
+    if(checkPieceOfOpponentOnElementNoDom(element, color)){
+      return true;
+    }
+  });
+
+  return returnArr;
 }
 
 // function to give highlight ids for queen
-function giveQueenHighlightIds(id) {
-  const rookMoves = giveRookHighlightIds(id);
+function giveQueenHighlightIds(id){
+  const rookMoves = giveRookHighlightIds(id)
   const bishopMoves = giveBishopHighlightIds(id);
   return {
-    left: rookMoves.left,
-    right: rookMoves.right,
-    top: rookMoves.top,
-    bottom: rookMoves.bottom,
-    topLeft: bishopMoves.topLeft,
-    topRight: bishopMoves.topRight,
-    bottomLeft: bishopMoves.bottomLeft,
-    bottomRight: bishopMoves.bottomRight,
-  };
+    "left": rookMoves.left,
+    "right" :rookMoves.right,
+    "top" : rookMoves.top,
+    "bottom" : rookMoves.bottom,
+    "topLeft": bishopMoves.topLeft,
+    "topRight": bishopMoves.topRight,
+    "bottomLeft" : bishopMoves.bottomLeft,
+    "bottomRight" : bishopMoves.bottomRight
+  }
+}
+
+function giveKingHighlightIds(id){
+  const rookMoves = giveRookHighlightIds(id)
+  const bishopMoves = giveBishopHighlightIds(id);
+  const returnResult =  {
+    "left": rookMoves.left,
+    "right" :rookMoves.right,
+    "top" : rookMoves.top,
+    "bottom" : rookMoves.bottom,
+    "topLeft": bishopMoves.topLeft,
+    "topRight": bishopMoves.topRight,
+    "bottomLeft" : bishopMoves.bottomLeft,
+    "bottomRight" : bishopMoves.bottomRight
+  }
+
+  for (const key in returnResult) {
+    if (Object.hasOwnProperty.call(returnResult, key)) {
+      const element = returnResult[key];
+
+      if(element.length != 0){
+        returnResult[key] = new Array(element[0]);
+      }
+      
+    }
+  }
+
+  return returnResult;
+}
+function giveKingCaptureIds(id, color){
+
+  if(!id) {
+    return [];
+  }
+
+  let result = giveKingHighlightIds(id);
+  result = Object.values(result).flat();
+  result = result.filter(element => {
+    if(checkPieceOfOpponentOnElementNoDom(element, color)){
+      return true;
+    }
+  })
+
+  return result;
 }
 
 export {
   checkPieceOfOpponentOnElement,
   checkSquareCaptureId,
+  giveKingCaptureIds,
   giveRookHighlightIds,
   giveQueenHighlightIds,
+  giveKingHighlightIds,
   giveKnightHighlightIds,
+  giveBishopCaptureIds,
+  giveQueenCapturesIds,
+  giveKnightCaptureIds,
   checkWeatherPieceExistsOrNot,
+  giveRookCapturesIds,
   giveBishopHighlightIds,
 };
--- Render/main.js
+++ Render/main.js
@@ -2,6 +2,8 @@ import * as piece from "../Data/pieces.js";
 import { ROOT_DIV } from "../Helper/constants.js";
 import { globalState } from "../index.js";
 
+const globalPiece = new Object();
+
 // function globalStateRender (this function is usefull to render pieces from globalStateData) => use when updating globalState
 function globalStateRender() {
   globalState.forEach((row) => {
@@ -23,26 +25,6 @@ function globalStateRender() {
   });
 }
 
-// move element to square with id
-function moveElement(piece, id) {
-  const flatData = globalState.flat();
-  flatData.forEach((el) => {
-    if (el.id == piece.current_position) {
-      delete el.piece;
-    }
-    if (el.id == id) {
-      el.piece = piece;
-    }
-  });
-  clearHightlight();
-  const previousPiece = document.getElementById(piece.current_position);
-  previousPiece.classList.remove("highlightYellow");
-  const currentPiece = document.getElementById(id);
-  currentPiece.innerHTML = previousPiece.innerHTML;
-  previousPiece.innerHTML = "";
-  piece.current_position = id;
-  // globalStateRender();
-}
 
 function selfHighlight(piece) {
   document
@@ -82,57 +64,93 @@ function initGameRender(data) {
       // render blackpawn
       if (square.id[1] == 7) {
         square.piece = piece.blackPawn(square.id);
+        globalPiece.black_pawn = square.piece
       }
 
       // render black rook
       if (square.id == "h8" || square.id == "a8") {
         square.piece = piece.blackRook(square.id);
+        if(globalPiece.black_rook_1) {
+          globalPiece.black_rook_2 = square.piece
+        } else {
+          globalPiece.black_rook_1 = square.piece
+        }
       }
 
       // render black knight
       if (square.id == "b8" || square.id == "g8") {
         square.piece = piece.blackKnight(square.id);
+        if(globalPiece.black_knight_1) {
+          globalPiece.black_knight_2 = square.piece
+        } else {
+          globalPiece.black_knight_1 = square.piece
+        }
       }
       // render black knight
       if (square.id == "c8" || square.id == "f8") {
         square.piece = piece.blackBishop(square.id);
+        if(globalPiece.black_bishop_1) {
+          globalPiece.black_bishop_2 = square.piece
+        } else {
+          globalPiece.black_bishop_1 = square.piece
+        }
       }
       // render black knight
       if (square.id == "d8") {
         square.piece = piece.blackQueen(square.id);
+        globalPiece.black_queen = square.piece
       }
       // render black knight
       if (square.id == "e8") {
         square.piece = piece.blackKing(square.id);
+        globalPiece.black_king = square.piece;
       }
 
       // render white pawn
       if (square.id[1] == 2) {
         square.piece = piece.whitePawn(square.id);
+        globalPiece.white_pawn = square.piece
       }
       // render white queen
       if (square.id == "d1") {
         square.piece = piece.whiteQueen(square.id);
+        globalPiece.white_queen = square.piece
       }
 
       // render white king
       if (square.id == "e1") {
         square.piece = piece.whiteKing(square.id);
+        globalPiece.white_king = square.piece;
       }
 
       // render white rook
       if (square.id == "h1" || square.id == "a1") {
         square.piece = piece.whiteRook(square.id);
+        if(globalPiece.white_rook_1) {
+          globalPiece.white_rook_2 = square.piece
+        } else {
+          globalPiece.white_rook_1 = square.piece
+        }
       }
 
       // render black knight
       if (square.id == "b1" || square.id == "g1") {
         square.piece = piece.whiteKnight(square.id);
+        if(globalPiece.white_knight_1) {
+          globalPiece.white_knight_2 = square.piece
+        } else {
+          globalPiece.white_knight_1 = square.piece
+        }
       }
 
       // render black bishop
       if (square.id == "c1" || square.id == "f1") {
         square.piece = piece.whiteBishop(square.id);
+        if(globalPiece.white_bishop_1) {
+          globalPiece.white_bishop_2 = square.piece
+        } else {
+          globalPiece.white_bishop_1 = square.piece
+        }
       }
 
       rowEl.appendChild(squareDiv);
@@ -174,6 +192,6 @@ export {
   renderHighlight,
   clearHightlight,
   selfHighlight,
-  moveElement,
   globalStateRender,
+  globalPiece
 };
